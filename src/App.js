69c6b2c59f274a43180bf2aee09f9cd303b8d656
andsimon3 <andsimon3@yandex.ru>
2020-09-02 22:38:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import './src/css/App.css';
 import GuestAva from './src/img/guest.svg';
 import ChooseArrow from './src/img/arrowdown.svg';
 import NewsIcon from './src/img/news.svg';
-import ProfileFragment from './src/fragments/Profile.js'
+//import ProfileFragment from './src/fragments/Profile.js'
 //{window.navigator.language.slice(0, 2)}
 //try{}
-
+function ProfileFragment() {
+  return(
+    <div>
+      Profile
+    </div>
+  );
+}
 var Username = 'Симонов Андрей';
 var Status = 'offline';
 function App() {
