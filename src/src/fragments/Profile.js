593ab5ba321a2bc4e97730191257089a1f81c5cb
andsimon3 <andsimon3@yandex.ru>
2020-09-05 15:42:17 +0300

    Update Profile.js

--- src/src/fragments/Profile.js
+++ src/src/fragments/Profile.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import '../css/Profile.css';
+import GuestAva from './src/img/guest.svg';
+
+var Username = 'Симонов Андрей';
+var Status = 'offline';
+
 
 function ProfileFragment(prop) {
 	return (
 		<div>
 			<div id='ProfileHeader'>
-				<img id='ProfileAvatar'/>
+				<img src={GuestAva} id='ProfileAvatar'/>
 				<span>
-					
+					{Username+' '+Status}
 				</span>
 			</div>
 			<div>
