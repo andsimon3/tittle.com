b276484ce88916a7e549fe6f7d89c2aa27a61716
andsimon3 <andsimon3@yandex.ru>
2020-09-06 10:06:56 +0300

    Update Profile.js

--- src/src/fragments/Profile.js
+++ src/src/fragments/Profile.js
@@ -7,6 +7,22 @@ var Status = 'offline';
 
 
 function ProfileFragment(prop) {
+	var xhr = new XMLHttpRequest();
+
+// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+xhr.open('GET', 'http://api.tittle.com/v0/test.php', false);
+
+// 3. Отсылаем запрос
+xhr.send();
+
+// 4. Если код ответа сервера не 200, то это ошибка
+if (xhr.status != 200) {
+  // обработать ошибку
+  alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+} else {
+  // вывести результат
+  alert( xhr.responseText ); // responseText -- текст ответа.
+}
 	return (
 		<div>
 			<div id='ProfileHeader'>
